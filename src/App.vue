e3a6f528dd8d9c783a8d5eddeb49af458b4bd980
RIQO RAHMA HIDAYAT <riqorhidayat@gmail.com>
2026-09-25 13:52:49 +0700

    fix: enable sticky navbar and anchor offset

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col font-sans overflow-x-hidden">
+  <div class="min-h-screen flex flex-col font-sans overflow-x-clip">
     <RouterView />
   </div>
 </template>
